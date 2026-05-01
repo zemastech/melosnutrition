98f315d66d261cc99e9903eaf18a4e028a8c18b3
Suralmk <surafelmelaku940@gmail.com>
2026-05-01 17:33:01 +0300

    chnage repo

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,9 +1,8 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const { name, phone, message } = await req.json();
 
   if (!name || !phone || !message) {
